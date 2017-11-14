4ed5e57ee9b5037b0f378c85d3019c6b1d172164
Paraparo <thomas.jacob.cohen@gmail.com>
2017-11-14 17:34:40 -0500

    Update Hangman.m

--- Hangman.m
+++ Hangman.m
@@ -1,9 +1,9 @@
 A = fileread('words.txt')
-loss = 6;
+loss = 0;
 wordChoice = randi([1,10], 1, 10);
-user = 0;
+user = 6;
 word = 'tristan';
-while user < loss
+while user > loss
     userInput = input('\nEnter char: ', 's');
     newStr = strrep(word, userInput, '');
     x = length(newStr);
@@ -13,7 +13,7 @@ while user < loss
         fprintf('\nCorrect! Guess again.\n');
         userInput;
     elseif x == y
-        user = user+1;
+        user = user-1;
         fprintf('\nIncorrect! You have made %1.0f incorrect guesses.\n', user);
     end
     
@@ -21,10 +21,4 @@ while user < loss
         break;
     end
     
-end
-
-if user == 6
-    fprintf('\nYou lose\n');
-else
-    fprintf('\nYou win');
 end
