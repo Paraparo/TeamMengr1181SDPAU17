75444781023cff7ac1ccdadd1a89515a05b33229
Tristanlopez <33469016+Tristanlopez@users.noreply.github.com>
2017-11-14 18:28:32 -0500

    Update Hangman.m

--- Hangman.m
+++ Hangman.m
@@ -1,9 +1,14 @@
-A = fileread('words.txt')
-loss = 0;
-wordChoice = randi([1,10], 1, 10);
-user = 6;
-word = 'tristan';
-while user > loss
+clc
+clear
+wordChoice = randi([1, 10], 1, 10);
+A = fileread('words.txt');
+split = strsplit(A);
+C=string(A)
+word=C(1:1,3)
+loss = 6;
+user = 0;
+
+while user < loss
     userInput = input('\nEnter char: ', 's');
     newStr = strrep(word, userInput, '');
     x = length(newStr);
@@ -13,13 +18,18 @@ while user > loss
         fprintf('\nCorrect! Guess again.\n');
         userInput;
     elseif x == y
-        run('HangmanDisplayChance.m')
-        user = user-1;
-        fprintf('\nIncorrect! You have  %1.0f guesses left.\n', user);
+        user = user+1;
+        fprintf('\nIncorrect! You have made %1.0f incorrect guesses.\n', user);
     end
     
     if isempty(word) == 1
         break;
     end
     
+end
+
+if user == 6
+    fprintf('\nYou lose\n');
+else
+    fprintf('\nYou win');
 end
